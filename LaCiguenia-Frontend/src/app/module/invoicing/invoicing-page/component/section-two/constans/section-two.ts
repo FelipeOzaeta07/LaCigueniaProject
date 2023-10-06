9c8e4c04c723c5039a1ac27e982c5854f5683624
EstebanVera3741 <estebanvera3741@gmail.com>
2023-10-05 22:29:29 -0500

    SENA-114 Indication Message when there are no Products on the Invoice - Frontend

---
+++ LaCiguenia-Frontend/src/app/module/invoicing/invoicing-page/component/section-two/constans/section-two.ts
@@ -0,0 +1,5 @@
+export const TEXT_TITLE = "¡Actualmente no tienes productos disponibles!";
+export const TEXT_ONE = "¡Haz clic ";
+export const TEXT_TWO = "aquí";
+export const TEXT_THREE = " para crearlos ahora mismo y comenzar a impulsar tu negocio!";
+export const SYMBOL = "$";
--- LaCiguenia-Frontend/src/app/module/invoicing/invoicing-page/component/section-two/section-two.component.ts
+++ LaCiguenia-Frontend/src/app/module/invoicing/invoicing-page/component/section-two/section-two.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChange
 import { ProductModel } from '@commons/domains/product/ProductModel';
 import { GenericResponse } from '@commons/response/GenericResponse';
 import { ReadProductsUseCase } from '@repository/product/case/ReadProductsUseCase';
+import { TEXT_ONE, TEXT_THREE, TEXT_TWO, SYMBOL, TEXT_TITLE  } from '@module/invoicing/invoicing-page/component/section-two/constans/section-two';
 
 @Component({
   selector: 'app-section-two',
@@ -14,9 +15,15 @@ export class SectionTwoComponent implements OnInit, OnChanges{
   @Output() selectProducts = new EventEmitter<ProductModel>();
   @Input() productSelector!: string;
 
+  textTitle = TEXT_TITLE;
+  textItemOne = TEXT_ONE;
+  textItemTwo = TEXT_TWO;
+  textItemThree = TEXT_THREE;
+  textSymbol = SYMBOL;
 
   product!: ProductModel [];
   productGroups: ProductModel[] = [];
+  message: boolean = true;
 
   constructor(private readProductsUseCase: ReadProductsUseCase){}
 
@@ -33,14 +40,21 @@ export class SectionTwoComponent implements OnInit, OnChanges{
   readProducts() {
     this.readProductsUseCase.execute().subscribe(
       (res: GenericResponse) => {
-        for(let item of res.objectResponse){
-          this.productGroups.push(item)
+        if(res.message != "Operación fallida"){
+          this.message = false;
+          for(let item of res.objectResponse){
+            this.productGroups.push(item)
+          }
         }
       }
     )
   }
 
-  selecProduct(i: number) {
-    this.selectProducts.emit(this.productGroups[i]);
+  selecProduct(productItem: ProductModel) {
+    const index = this.productGroups.findIndex(item => item.productId === productItem.productId);
+
+    if (index !== -1) {
+      this.selectProducts.emit(this.productGroups[index]);
+    }
   }
 }
